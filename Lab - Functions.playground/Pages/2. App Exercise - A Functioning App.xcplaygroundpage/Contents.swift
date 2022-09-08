390022d2d9ff3fe70c7ef796b3293442abca2640
AnkithJG <77761673+AnkithJG@users.noreply.github.com>
2022-09-08 09:25:49 -0500

    Update Contents.swift

--- Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
+++ Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -3,17 +3,51 @@
  
  >These exercises reinforce Swift concepts in the context of a fitness tracking app.
  
- As you may have guessed, functions are key to making your app work. For example, in every exercise dealing with step count until now, you have simply assigned a number of steps to a `steps` variable. This isn't very realistic seeing as the number of steps you take increments one at a time and continues changing throughout the day.
+ As you may have guessed, functions are key to making your app work. 
+For example, in every exercise dealing with step count until now, you have simply assigned a number of steps to a `steps` variable. 
+This isn't very realistic seeing as the number of steps you take increments one at a time and continues changing throughout the day.
  
- A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
+ A reoccurring process like this is a perfect candidate for a function. 
+Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. 
+Call the function multiple times and observe the printouts.
  */
 var steps = 0
+func incrementSteps(){
+ steps++
+ print(steps)
+}
 
+incrementSteps()
+incrementSteps()
+incrementSteps()
+incrementSteps()
 
 /*:
- Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout. Remember, you can convert numbers using the appropriate Int or Double initializer.
+ Similarly, if you want to regularly provide progress updates to your user, 
+you can put your control flow statements that check on progress into a function. 
+Write a function called `progressUpdate` after the declaration of `goal` below. 
+The function should print "You're off to a good start." 
+if `steps` is less than 10% of `goal`, "You're almost halfway there!" 
+if `steps` is less than half of `goal`, "You're over halfway there!" 
+if `steps` is less than 90% of `goal`, "You're almost there!" 
+if `steps` is less than `goal`, and "You beat your goal!" otherwise. 
+Call the function and observe the printout. Remember, you can convert numbers using the appropriate Int or Double initializer.
  */
-let goal = 10000
+let goal: Double = 10000
+func progressUpdate(){
+ if(steps > (.10 * goal)){
+  print("You're off toa good start!")
+ }
+ else if(steps > (.50 * goal)){
+  print("You're over halfway there!")
+ }
+}
+ else if(steps > (.90 * goal)){
+  print("You're almost there!")
+ }
+ else if(steps > goal){
+  print("You beat your goal!")
+ }
+// 
+ }: [Previous](@previous)  |  page 2 of 6  |  [Next: Exercise - Parameters and Argument Labels](@next)
 
-
-//: [Previous](@previous)  |  page 2 of 6  |  [Next: Exercise - Parameters and Argument Labels](@next)
